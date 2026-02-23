e0487730958c89e48411a2354412767161d7cdda
Jana <janaahmedsaad@gmail.com>
2026-02-23 10:46:27 +0200

    setup & main components generation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss',
+  standalone: true,
 })
 export class AppComponent {
   title = 'analytics-dashboard';
---
+++ src/app/features/widgets/kpi-gauge/kpi-gauge.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-kpi-gauge',
+  imports: [],
+  templateUrl: './kpi-gauge.component.html',
+  styleUrl: './kpi-gauge.component.css',
+})
+export class KpiGaugeComponent {
+
+}
